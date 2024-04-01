f5abda446ad18197ef89e122ef464d83a82e3a1b
CKIRUser <CKIRUser@192.34.55.25>
2024-04-01 23:05:45 +0900

    .

--- BACKJUN_JAVA/src/JAVA_FOR/_10950.java
+++ BACKJUN_JAVA/src/JAVA_FOR/_10950.java
@@ -26,10 +26,6 @@ import java.util.Scanner;
 
 public class _10950 {
 	  
-	/**
-	 * 메인 메서드
-	 * @param args 명령행 인수
-	 */
 	public static void main(String[] args) {  
 		Scanner sc = new Scanner(System.in) ;
 		
