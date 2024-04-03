8bdc3bf584e268786d9d88bcd48963ee20271fce
CKIRUser <CKIRUser@192.34.55.25>
2024-04-03 12:43:42 +0900

    25313 ING

---
+++ BACKJUN_JAVA/src/JAVA_FOR/A5_25313.java
@@ -0,0 +1,23 @@
+ // 백준 단계별로풀기[FOR문]  
+ // 반복문 5번 [백준25314]
+ // 문제
+ // 본문 내용이 길어 본문 참고 ,,
+ // https://www.acmicpc.net/problem/25314
+ // 
+
+package JAVA_FOR;
+
+import java.util.Scanner;
+
+public class A5_25313 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in) ;
+		
+		
+		
+
+	}
+
+}
