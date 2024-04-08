675d6899fa133370bf69c56420ff0173dcfc8ab7
CKIRUser <CKIRUser@192.34.55.25>
2024-04-08 22:20:16 +0900

    27866DONE

---
+++ BACKJUN_JAVA/src/JAVA_String/A1_27866.java
@@ -0,0 +1,17 @@
+package JAVA_String;
+
+import java.util.Scanner;
+
+public class A1_27866 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in) ;
+		
+		String str = sc.next();
+		int i = sc.nextInt() -1 ;
+		
+		System.out.println(str.charAt(i));
+	}
+
+}
